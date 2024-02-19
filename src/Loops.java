b71bafce08a91da16f72e958c10b44b52e4c60ac
91704 <vishal.sharma_bca22@gla.ac.in>
2024-02-19 23:32:40 +0530

    1 1 2 1 2 3

--- src/Loops.java
+++ src/Loops.java
@@ -173,7 +173,7 @@
         }
     }
     */
-class Loops
+/*class Loops
     {
         public static void main(String a[])
         {
@@ -188,4 +188,19 @@ class Loops
             }
 
         }
+    }
+
+ */
+class Loops {
+    public static void main(String a[])
+    {
+        int i,j;
+        for (i=1; i<=5; i++){
+            for (j=1; j<=i; j++){
+                System.out.print(j +" ");
+            }
+        System.out.println();
+        }
+
+    }
     }
