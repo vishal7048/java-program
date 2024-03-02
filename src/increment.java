74027d5cae803ba4f8d440ad9b773b0dbc85c1b0
91704 <vishal.sharma_bca22@gla.ac.in>
2024-03-02 23:43:01 +0530

    output and checking code ..

--- src/increment.java
+++ src/increment.java
@@ -1,4 +1,4 @@
-public class increment {
+/*public class increment {
     public static void main(String[] args) {
         int a=5;
         int b=--a;
@@ -9,3 +9,16 @@ public class increment {
         System.out.println(e);
     }
 }
+ */
+/*class My {
+    static int a=20;
+    My(){
+        a=200;
+    }
+    public static void main(String args[])
+    {
+        new My();
+        System.out.println(a);
+    }
+}*/
+
